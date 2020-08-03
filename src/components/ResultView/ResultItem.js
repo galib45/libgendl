fbd1c81caa183095a8abb17900c75a0c08bed1c2
galib45 <asadulgalibvat69@gmail.com>
2020-08-03 12:21:44 +0600

    fixed padding on result-item

--- src/components/ResultView/ResultItem.js
+++ src/components/ResultView/ResultItem.js
@@ -4,14 +4,14 @@ export default function ResultItem(props) {
     return (
         <div className="result-item">
             <div className="row">
-                <div className="col-5">
+                <div className="col-4">
                     <div className="result-img">
                         <a href={props.item.link}>
                             <img src={props.item.img} alt="cover"/>
                         </a>
                     </div>
                 </div>
-                <div className="col-7">
+                <div className="col-8">
                     <div className="result-desc">
                         <p>{props.item.title}</p> 
                         <p>{props.item.author}</p>
